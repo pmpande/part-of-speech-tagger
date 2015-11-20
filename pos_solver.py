499f4be68bfe57e442e0192072a6831bd0245455
Pranav Pande <pmpande@iu.edu>
2015-11-20 04:21:33 -0500

    Added Naive Approach

--- pos_solver.py
+++ pos_solver.py
@@ -21,18 +21,92 @@ class Solver:
 
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
+    transition = {}
+    emission = {}
+    initial_state_distribution = {}
+    prob_speech = {}
+    part_of_speech = {}
+    transitions = 0
+    word = {}
+
     def posterior(self, sentence, label):
         return 0
 
     # Do the training!
     #
     def train(self, data):
-        pass
+        self.transition = {}
+        self.emission = {}
+        self.word = {}
+        self.part_of_speech = {}
+        self.transitions = 0
+        for row in data:
+            for x in range(0, len(row[0])):
 
+                if row[0][x] not in self.word:
+                    self.word[row[0][x]] = 1.0
+                else:
+                    self.word[row[0][x]] += 1.0
+
+                if row[1][x] not in self.part_of_speech:
+                    self.part_of_speech[row[1][x]] = 1.0
+                else:
+                    self.part_of_speech[row[1][x]] += 1.0
+
+                if x == 0:
+                    if row[1][x] not in self.initial_state_distribution:
+                        self.initial_state_distribution[row[1][x]] = 1.0
+                    else:
+                        self.initial_state_distribution[row[1][x]] += 1.0
+                else:
+                    inp_string = (row[1][x-1], row[1][x])
+
+                    if inp_string not in self.transition:
+                        self.transition[inp_string] = 1.0
+                        self.transitions += 1.0
+                    else:
+                        self.transition[inp_string] += 1.0
+                        self.transitions += 1.0
+
+                if (row[0][x], row[1][x]) not in self.emission:
+                    self.emission[(row[0][x], row[1][x])] = 1.0
+                else:
+                    self.emission[(row[0][x], row[1][x])] += 1.0
+        for row in self.word.keys():
+            for column in self.part_of_speech.keys():
+                if (row, column) in self.emission:
+                    self.emission[(row, column)] /= self.part_of_speech[column]
+        for x in self.part_of_speech.keys():
+            self.initial_state_distribution[x] /= len(data)
+        for x in self.part_of_speech.keys():
+            self.prob_speech[x] = self.part_of_speech[x] / sum(self.part_of_speech.values())
+        for x in self.part_of_speech.keys():
+            for y in self.part_of_speech.keys():
+                if (x, y) not in self.transition:
+                    self.transition[(x, y)] = 1e-10
+                else:
+                    self.transition[(x, y)] /= self.part_of_speech[x]
+        print "Training Complete"
     # Functions for each algorithm.
     #
     def naive(self, sentence):
-        return [ [ [ "noun" ] * len(sentence)], [] ]
+        max_s = None
+        pos = None
+        output_list = []
+        for word in sentence:
+            max_s = 0.0
+            for speech in self.part_of_speech.keys():
+                if (word, speech) not in self.emission:
+                    prob_each = self.emission[(word, speech)] = 0
+                else:
+                    prob_each = self.emission[(word, speech)] * self.prob_speech[speech]
+                if prob_each > max_s:
+                    max_s = prob_each
+                    pos = speech
+            if max_s is 0.0:
+                pos = 'noun'
+            output_list.append(pos)
+        return [ [output_list], [] ]
 
     def mcmc(self, sentence, sample_count):
         return [ [ [ "noun" ] * len(sentence) ] * sample_count, [] ]
