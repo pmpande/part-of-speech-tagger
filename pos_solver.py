81e6897808943da084bdd211aeb2ceff06db3f42
Pranav Pande <pmpande@iu.edu>
2015-11-22 18:50:21 -0500

    Added documentation

--- pos_solver.py
+++ pos_solver.py
@@ -2,13 +2,79 @@
 # CS B551 Fall 2015, Assignment #5
 #
 # Your names and user ids:
+# 1. Nishant Shah (nishshah)
+# 2. Pranav Pande (pmpande)
 #
 # (Based on skeleton code by D. Crandall)
 #
+####--------------------------------------------------------------------------------------------------------------------
+# Report:
+# 1. Code Description:
+# For finding P(S1), P(Si+1|Si) and P(Wi|Si) we have used maximum likelihood estimation.
+# P(S1) = count(number of times Si appaered at first position) / count(total number of sentences)
+# P(Si+1|Si) = count(Si, Si+1) / count(Si)
+# P(Wi|Si) = count(Wi, Si) / count(Si)
+# Apart from above we also have dictionary of 1. Words with total count of each word - word[word i]
+#                                             2. Part of speech with total count for each POS - part_of_speech[speech i]
+#                                             3. Probability of each POS in total POS - prob_speech[speech i]
+# For storing probabilities we have used dictionary as in following format
+# P(Si+1|Si) - transition[(speech i-1, speech i)]
+# P(S1) - initial_probability_distribution[speech i]
+# P(Wi|Si) - emission[(word i, speech i)]
 #
-####
-# Put your report here!!
-####
+# Naive:
+# Here we are selecting a POS by finding most probable tag for each word.
+#
+# Sampler:
+#
+# Max Marginal:
+#
+# MAP:
+# We have first calculated prob. of system in state 0 with each POS. Then finding values for each state by
+# multiplying emission value with maximum of product of value of last state and transition prob. from last state to
+# current state with each POS. At each state for each POS we are also storing the speech with maximum probability. Thus
+# to get find most probable sequence we are selecting values of POS based on POS tagged at current state using its
+# stored value.
+#
+# Best:
+# Here we are using Viterbi as our best algorithm. With fined tuned Viterbi(as explained in assumptions section) we are
+# getting our best results.
+#
+# Comments are provided for each function for better understanding
+# ----------------------------------------------------------------------------------------------------------------------
+# 2. Results: Scored 2000 sentences with 29442 words.
+#                       Words correct:     Sentences correct:
+#   0. Ground truth:      100.00%              100.00%
+#          1. Naive:       93.92%               47.45%
+#        2. Sampler:       18.60%                0.00%
+#   3. Max marginal:       18.60%                0.00%
+#            4. MAP:       95.69%               57.90%
+#           5. Best:       95.69%               57.90%
+#
+#-----------------------------------------------------------------------------------------------------------------------
+#
+# 3. Challenges and Assumptions:
+#
+#   Challenge: Taking decisions for unknown words
+#   Assumptions:
+#   1. Setting probabilities of unknown words to either zero or a very small value:
+#   Here we were facing challenges when any new word comes which is not present in the training corpus. For such words
+# we have set their probability to zero(0) or to a very small value(1e-10). In naive while calculating emission prob.
+# such words we are assigning its value as zero. Whereas in Viterbi we have assigned emission value and prob. value of
+# states as 1e-10 if it is zero.
+#   2. Assigning Transition values for unknown transitions to a very small probability:
+#   While learning the corpus, if there does not exists any transition from some POS to another then we are assigning
+# its value as 1e-10 so that keeping a small probability of its happening.
+#
+#   Challenge: Fine tuning Viterbi to get best results
+#   Assumptions:
+#   1. Assigning a very small value when value of the state is zero:
+#   If any state is getting its maximum value for the product of last state value and transition value as zero then for
+# such states we are assigning its default state value as 1e-10.
+#   2. Assigning POS tag of noun when value of state is zero:
+#   If any state is getting its maximum value for the product of last state value and transition value as zero then for
+# such states we are assigning its default tag as 'noun'.
+####--------------------------------------------------------------------------------------------------------------------
 
 import random
 import math
@@ -55,18 +121,19 @@ class Solver:
         self.part_of_speech = {}
         self.transitions = 0
         for row in data:
+            #learning from each word of given sentence
             for x in range(0, len(row[0])):
-
+                #Counting frequency of each word
                 if row[0][x] not in self.word:
                     self.word[row[0][x]] = 1.0
                 else:
                     self.word[row[0][x]] += 1.0
-
+                #Counting frequency of each POS
                 if row[1][x] not in self.part_of_speech:
                     self.part_of_speech[row[1][x]] = 1.0
                 else:
                     self.part_of_speech[row[1][x]] += 1.0
-
+                #Counting frequency of each POS for appearing at the start of sentence
                 if x == 0:
                     if row[1][x] not in self.initial_state_distribution:
                         self.initial_state_distribution[row[1][x]] = 1.0
@@ -74,26 +141,30 @@ class Solver:
                         self.initial_state_distribution[row[1][x]] += 1.0
                 else:
                     inp_string = (row[1][x-1], row[1][x])
-
+                    #Counting frequency of each transition for each POS
                     if inp_string not in self.transition:
                         self.transition[inp_string] = 1.0
                         self.transitions += 1.0
                     else:
                         self.transition[inp_string] += 1.0
                         self.transitions += 1.0
-
+                #Counting frequency of each word with each POS as appearing in the sentences
                 if (row[0][x], row[1][x]) not in self.emission:
                     self.emission[(row[0][x], row[1][x])] = 1.0
                 else:
                     self.emission[(row[0][x], row[1][x])] += 1.0
         for row in self.word.keys():
             for column in self.part_of_speech.keys():
+                #Calculating emission probability
                 if (row, column) in self.emission:
                     self.emission[(row, column)] /= self.part_of_speech[column]
+        #Calculating initial state distribution
         for x in self.part_of_speech.keys():
             self.initial_state_distribution[x] /= len(data)
+        #calculating probability of existence os each POS in the data
         for x in self.part_of_speech.keys():
             self.prob_speech[x] = self.part_of_speech[x] / sum(self.part_of_speech.values())
+        #calculating probability for each transition transition(state i-1, state i)
         for x in self.part_of_speech.keys():
             for y in self.part_of_speech.keys():
                 if (x, y) not in self.transition:
@@ -104,7 +175,6 @@ class Solver:
     # Functions for each algorithm.
     #
     def naive(self, sentence):
-        max_s = None
         pos = None
         output_list = []
         for word in sentence:
@@ -114,9 +184,11 @@ class Solver:
                     prob_each = self.emission[(word, speech)] = 0
                 else:
                     prob_each = self.emission[(word, speech)] * self.prob_speech[speech]
+                #storing max probability and respective POS
                 if prob_each > max_s:
                     max_s = prob_each
                     pos = speech
+            #if probability is zero then tag 'noun' POS
             if max_s is 0.0:
                 pos = 'noun'
             output_list.append(pos)
@@ -127,13 +199,17 @@ class Solver:
 
     def best(self, sentence):
         output_list = []
+        #dictioanary for saving value for states
         v = {}
+        #dictionary for saving POS tag while calculating maximum probability for each state
         sequence = {}
+        #finding values for state 0 for each POS for first word of the sequence
         for speech in self.part_of_speech.keys():
             if (sentence[0], speech) not in self.emission:
                 self.emission[(sentence[0], speech)] = 1e-10
             prob = self.initial_state_distribution[speech] * self.emission[(sentence[0], speech)]
             v[(speech, 0)] = prob
+        #finding value for each state
         for x in range(1, len(sentence)):
             for speech1 in self.part_of_speech.keys():
                 max_prob = 0.0
@@ -143,19 +219,24 @@ class Solver:
                     if prob > max_prob:
                         max_prob = prob
                         max_speech = speech2
+                #if probability is zero then tag 'noun' POS
                 if max_prob == 0:
                     max_prob = 1e-10
                     max_speech = 'noun'
                 v[(speech1, x)] = self.emission[(sentence[x], speech1)] * max_prob
+                #storing POS with maximum probability for the state
                 sequence[(speech1, x)] = max_speech
         tn = len(sentence) - 1
         max = 0.0
+        #finding maximum probability for last state
         for speech in self.part_of_speech.keys():
             if v[(speech, tn)] > max:
                 max = v[(speech, tn)]
                 max_speech = speech
         output_list.append(max_speech)
         tn = len(sentence)
+        #tracking values of POS from last state to first by getting saved tag while finding maximum prob. of each state
+        # with some POS
         for x in range(0, tn-1):
             speech = sequence[(max_speech, tn - x - 1)]
             max_speech = speech
@@ -165,18 +246,23 @@ class Solver:
         return [ [output_list], [] ]
 
 
+
     def max_marginal(self, sentence):
         return [ [ [ "noun" ] * len(sentence)], [[0] * len(sentence),] ]
 
     def viterbi(self, sentence):
         output_list = []
+        #dictioanary for saving value for states
         v = {}
+        #dictionary for saving POS tag while calculating maximum probability for each state
         sequence = {}
+        #finding values for state 0 for each POS for first word of the sequence
         for speech in self.part_of_speech.keys():
             if (sentence[0], speech) not in self.emission:
                 self.emission[(sentence[0], speech)] = 1e-10
             prob = self.initial_state_distribution[speech] * self.emission[(sentence[0], speech)]
             v[(speech, 0)] = prob
+        #finding value for each state
         for x in range(1, len(sentence)):
             for speech1 in self.part_of_speech.keys():
                 max_prob = 0.0
@@ -186,19 +272,24 @@ class Solver:
                     if prob > max_prob:
                         max_prob = prob
                         max_speech = speech2
+                #if probability is zero then tag 'noun' POS
                 if max_prob == 0:
                     max_prob = 1e-10
                     max_speech = 'noun'
                 v[(speech1, x)] = self.emission[(sentence[x], speech1)] * max_prob
+                #storing POS with maximum probability for the state
                 sequence[(speech1, x)] = max_speech
         tn = len(sentence) - 1
         max = 0.0
+        #finding maximum probability for last state
         for speech in self.part_of_speech.keys():
             if v[(speech, tn)] > max:
                 max = v[(speech, tn)]
                 max_speech = speech
         output_list.append(max_speech)
         tn = len(sentence)
+        #tracking values of POS from last state to first by getting saved tag while finding maximum prob. of each state
+        # with some POS
         for x in range(0, tn-1):
             speech = sequence[(max_speech, tn - x - 1)]
             max_speech = speech
